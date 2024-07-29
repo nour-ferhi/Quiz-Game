393526d2b4a6e08ac664038ab43d97cf60ae1d26
nour-ferhi <ferhinour@dopacode.com>
2024-07-29 22:25:25 +0100

    add questions and options

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 # Functions definition
 def new_game():
-    pass
+    guesses= []
+    correct_guesses= 0
+    question_num= 1
+
+    for key in questions:
+        print("-------")
+        print(key)
 def check_answer():
     pass
 def display_score():
     pass
 def play_again():
     pass
+
+questions= {
+    "who created python?: " : "A",
+    "what year was python created?: " : "B",
+    "is the earth round?: ": "A" 
+}
+
+options= [
+    ["A. Guido van Rossum","B. Ferhi Nour","C. Lionel Messi"],
+    ["A. 2020","B. 1991","C. 2011"],
+    ["A. True","B. False"]
+]
+
+
+new_game()
