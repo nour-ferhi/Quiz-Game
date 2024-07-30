3376f3353aa6e8edc98f9ff3920a685639d2e84c
nour-ferhi <ferhinour@dopacode.com>
2024-07-30 18:44:51 +0100

    Final Commit

--- main.py
+++ main.py
@@ -7,23 +7,51 @@ def new_game():
     for key in questions:
         print("-------")
         print(key)
-def check_answer():
-    pass
-def display_score():
-    pass
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("A,B,C or D: ").upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key),guess)
+        question_num+=1
+    display_score(correct_guesses,guesses)
+
+def check_answer(answer,guess):
+    if answer == guess:
+        print("Correct")
+        return 1
+    else:
+        print("Incorrect")
+        return 0
+def display_score(correct_guesses,guesses):
+    print("----------------------------------")
+    print("Results")
+    print("----------------------------------")
+    print("Answers: ",end="")
+    for i in questions:
+        print(questions.get(i),end=" ")
+    print()
+    print("Answers: ",end="")
+    for i in guesses:
+        print(i,end=" ")
+    print()
+    score= int((correct_guesses / len(questions))*100)
+    print("Your Score: "+str(score)+"%")
 def play_again():
     pass
 
 questions= {
     "who created python?: " : "A",
     "what year was python created?: " : "B",
-    "is the earth round?: ": "A" 
+    "is the earth round?: ": "A" ,
+    "Python is ":"D"
 }
 
 options= [
-    ["A. Guido van Rossum","B. Ferhi Nour","C. Lionel Messi"],
-    ["A. 2020","B. 1991","C. 2011"],
-    ["A. True","B. False"]
+    ["A. Guido van Rossum","B. Ferhi Nour","C. Lionel Messi","D.Elon Musk"],
+    ["A. 2020","B. 1991","C. 2011","D. 2000"],
+    ["A. True","B. False","C. I don't know","D. Sometimes"],
+    ["A. CEO","B. Football Player","C. Developer","D. Programming language"]
 ]
 
 
